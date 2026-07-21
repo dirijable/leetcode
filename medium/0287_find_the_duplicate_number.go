52a7267505645a1417694bc06317445db9552b11
Dirijable <checkpuk1379@gmail.com>
2026-07-21 13:00:42 +0300

    1 medium

---
+++ medium/0287_find_the_duplicate_number.go
@@ -0,0 +1,13 @@
+package medium
+
+func findDuplicate(nums []int) int {
+	slow, fast := nums[0], nums[nums[0]]
+	for slow != fast {
+		slow, fast = nums[slow], nums[nums[fast]]
+	}
+	slow = 0
+	for slow != fast {
+		slow, fast = nums[slow], nums[fast]
+	}
+	return slow
+}
