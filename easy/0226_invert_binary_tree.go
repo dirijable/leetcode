4c07b6dabc7b63b3152dd8a9a30b0ab87ff014a7
Dirijable <checkpuk1379@gmail.com>
2026-07-23 21:05:53 +0300

    2 easy

---
+++ easy/0226_invert_binary_tree.go
@@ -0,0 +1,21 @@
+package easy
+
+type TreeNode struct {
+    Val int
+    Left *TreeNode
+    Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+    invert(root)
+	return root
+}
+
+func invert(root *TreeNode) {
+	if root == nil {
+		return
+	} 
+	root.Left, root.Right = root.Right, root.Left
+	invert(root.Left)
+	invert(root.Right)
+}
